d31de16f020ef075b89407d755b8eb8804c55a68
cmvn-linhnguyen <linh.nguyen@classmethod.vn>
2023-08-16 00:50:36 +0700

    fix comment

--- index.js
+++ index.js
@@ -26,118 +26,94 @@ var products = [
   },
 ];
 
-var products2 = [
-  {
-    id: 5,
-    image: './assets/images/product-1.png',
-    name: 'T-Shirt Summer Vibes',
-    price: 119.99,
-    discount: 30,
-  },
-  {
-    id: 6,
-    image: './assets/images/product-2.png',
-    name: 'Loose Knit 3/4 Sleeve',
-    price: 119.99,
-  },
-  {
-    id: 7,
-    image: './assets/images/product-3.png',
-    name: 'Basic Slim Fit T-Shirt',
-    price: 79.99,
-  },
-  {
-    id: 8,
-    image: './assets/images/product-4.png',
-    name: 'Loose Textured T-Shirt',
-    price: 119.99,
-  },
-];
+//Count the number of products in the cart
+function updateCartQty() {
+  var qtyElem = document.getElementById('cart-quantity');
+  var cartItems = JSON.parse(localStorage.getItem('cart')) || [];
 
-function countQuantity() {
-  var cart_quantity = document.getElementById('cart-quantity');
-  if (localStorage.getItem('cart')) {
-    cart_quantity.innerHTML = JSON.parse(localStorage.getItem('cart')).reduce(
-      (sum, i) => {
-        return (sum += i.quantity);
-      },
-      0
-    );
-  } else cart_quantity.innerHTML = '0';
+  var totalQty = cartItems.reduce((sum, item) => sum + item.quantity, 0);
+  qtyElem.innerHTML = totalQty.toString();
 }
 
-function createList(element_id, products) {
-  var ul = document.getElementById(element_id);
+function addToCart(product) {
+  var cart = JSON.parse(localStorage.getItem('cart')) || [];
+  var index = cart.findIndex((item) => item.id === product.id);
 
-  if (ul) {
-    products.forEach((product) => {
-      var li = document.createElement('li');
-      li.setAttribute('class', 'col col-3 col-sm-6');
-
-      var a = document.createElement('a');
-      a.setAttribute('class', 'product-link');
-
-      var product_wrapper = document.createElement('div');
-      product_wrapper.setAttribute('class', 'product-item flex-column');
-
-      var image_wrapper = document.createElement('div');
-      image_wrapper.setAttribute('class', 'image-wrapper');
-
-      var image = document.createElement('img');
-      image.setAttribute('class', 'img product-image');
-      image.setAttribute('src', product.image);
-      image.setAttribute('alt', 'T-Shirt Summer Vibes');
-
-      var h4 = document.createElement('h4');
-      h4.setAttribute('class', 'product-name');
-      h4.innerHTML = product.name;
-
-      var p = document.createElement('p');
-      p.setAttribute('class', 'product-price');
-      p.innerHTML = product.price;
-
-      var button = document.createElement('button');
-      button.setAttribute('class', 'product-button');
-      button.innerHTML = 'Add to Cart';
-      button.addEventListener('click', function addToCart() {
-        var listProducts = localStorage.getItem('cart');
-
-        if (listProducts) {
-          listProducts = JSON.parse(listProducts);
-
-          var index = listProducts.findIndex((obj) => obj.id === product.id);
-
-          if (index === -1) {
-            listProducts.push({ ...product, quantity: 1 });
-          } else {
-            listProducts[index].quantity += 1;
-          }
-          localStorage.setItem('cart', JSON.stringify(listProducts));
-        } else {
-          localStorage.setItem(
-            'cart',
-            JSON.stringify([{ ...product, quantity: 1 }])
-          );
-        }
-
-        countQuantity();
-      });
-
-      image_wrapper.appendChild(image);
-      image_wrapper.appendChild(button);
-
-      product_wrapper.appendChild(image_wrapper);
-      product_wrapper.appendChild(h4);
-      product_wrapper.appendChild(p);
-
-      a.appendChild(product_wrapper);
-      li.appendChild(a);
-
-      ul.appendChild(li);
-    });
+  if (index === -1) {
+    cart.push({ ...product, quantity: 1 });
+  } else {
+    cart[index].quantity += 1;
   }
+
+  localStorage.setItem('cart', JSON.stringify(cart));
+  updateCartQty();
 }
 
-countQuantity();
-createList('recommendation-list', products);
-createList('product-today', products2);
+//Render list products
+function renderProductList(products) {
+  var listItem = document.createElement('ul');
+  listItem.setAttribute('class', 'product-list flex-row row');
+
+  products.forEach((product) => {
+    var item = document.createElement('li');
+    item.className = 'col col-3 col-sm-6';
+
+    var productLink = document.createElement('a');
+    productLink.className = 'product-link';
+
+    var productItem = document.createElement('div');
+    productItem.className = 'product-item flex-column';
+
+    var imageWrapper = document.createElement('div');
+    imageWrapper.className = 'image-wrapper';
+
+    var productImage = document.createElement('img');
+    productImage.className = 'img product-image';
+    productImage.src = product.image;
+    productImage.alt = product.name;
+
+    var productName = document.createElement('h4');
+    productName.className = 'product-name';
+    productName.innerHTML = product.name;
+
+    var productPrice = document.createElement('p');
+    productPrice.className = 'product-price';
+    productPrice.innerHTML = product.price;
+
+    var addButton = document.createElement('button');
+    addButton.className = 'product-button';
+    addButton.innerHTML = 'Add to Cart';
+    addButton.addEventListener('click', function () {
+      addToCart(product);
+    });
+
+    imageWrapper.appendChild(productImage);
+    imageWrapper.appendChild(addButton);
+
+    productItem.appendChild(imageWrapper);
+    productItem.appendChild(productName);
+    productItem.appendChild(productPrice);
+
+    productLink.appendChild(productItem);
+    item.appendChild(productLink);
+
+    listItem.appendChild(item);
+  });
+
+  return listItem;
+}
+
+function displayProducts() {
+  updateCartQty();
+  var productList = renderProductList(products);
+
+  var productsWrap1 = document.getElementsByClassName('products-wrap')[0];
+  var productsWrap2 = document.getElementsByClassName('products-wrap')[1];
+
+  var productListCopy = productList.cloneNode(true);
+
+  productsWrap1.appendChild(productList);
+  productsWrap2.appendChild(productListCopy);
+}
+
+displayProducts();
